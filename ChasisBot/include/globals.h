2fb589d90f80422ec89816aa25343546eeb669ff
Oliver Juhl <oliver.juhl@mines.sdsmt.edu>
2022-10-22 11:13:40 -0600

    added global declarations for the motors for chassisbot

---
+++ ChasisBot/include/globals.h
@@ -0,0 +1,18 @@
+#ifndef GLOBALS
+#define GLOBALS
+
+#include "api.h"
+
+
+extern pros::Motor rightFrontDrive;
+extern pros::Motor rightMidDrive;
+extern pros::Motor rightBackDrive;
+extern pros::Motor leftFrontDrive;
+extern pros::Motor leftMidDrive;
+extern pros::Motor leftBackDrive;
+
+extern pros::IMU inertial;
+
+
+
+#endif
