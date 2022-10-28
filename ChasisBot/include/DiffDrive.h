19e88b682c9f19465ac0a774ea6cbbe25b1fe264
Oliver Juhl <oliver.juhl@mines.sdsmt.edu>
2022-10-27 22:03:06 -0600

    Added diff drive skeleton

---
+++ ChasisBot/include/DiffDrive.h
@@ -0,0 +1,18 @@
+#ifndef DIFF_DRIVE
+#define DIFF_DRIVE
+
+/*class DiffDrive
+{
+
+    public:
+        void driveTiles(double target, bool waitForCompletion=true);
+        void turnDegreesAbsolute(double target, bool waitForCompletion);
+        void turnDegreesRelative();
+
+
+
+
+};*/
+
+#endif
+
