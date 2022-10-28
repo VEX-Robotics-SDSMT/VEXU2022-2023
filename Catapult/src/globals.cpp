3f04d22d8c14a451d9af51f3e5f657ad1f13d5ac
Henderson, Brandon G. - SDSMT Student <hendersongbrandon@gmail.com>
2022-10-28 11:12:04 -0600

    Changed right catapult direction
    Deon requested I reverse the direction of the right catapult motor seeing as he made a change to the bot.

--- Catapult/src/globals.cpp
+++ Catapult/src/globals.cpp
@@ -15,7 +15,7 @@ pros::Motor rightBotRearDrive(R_BOT_REAR, pros::E_MOTOR_GEARSET_06, true);
 pros::Motor rightFrontDrive(R_FRONT, pros::E_MOTOR_GEARSET_06, false);
 
 pros::Motor leftCatapult(R_CATAPULT, pros::E_MOTOR_GEARSET_36, true);
-pros::Motor rightCatapult(L_CATAPULT, pros::E_MOTOR_GEARSET_36, false);
+pros::Motor rightCatapult(L_CATAPULT, pros::E_MOTOR_GEARSET_36, true);
 
 pros::Motor intake(INTAKE, pros::E_MOTOR_GEARSET_06, true);
 
