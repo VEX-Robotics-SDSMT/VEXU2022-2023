0c6b3afd5d3501c8cd8cdd6a08609898e7f1c172
Henderson, Brandon G. - SDSMT Student <hendersongbrandon@gmail.com>
2023-02-20 20:16:18 -0700

    documentation

--- Flywheel/src/controllerUI.cpp
+++ Flywheel/src/controllerUI.cpp
@@ -13,7 +13,7 @@ int initAutonSide(pros::Controller& Master)
 			Master.print(0, 0, "RED SELECTED");
 			pros::delay(1000);
 			Master.clear_line(0);
-			return 1;
+			return 1; // Output coresponding to red
 		}
 		else if(Master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X))
 		{
@@ -22,7 +22,7 @@ int initAutonSide(pros::Controller& Master)
 			Master.print(0, 0, "BLUE SELECTED");
 			pros::delay(1000);
 			Master.clear_line(0);
-			return 2;
+			return 2; // Output corresponding to blue
 		}
         pros::delay(20);
 	}
